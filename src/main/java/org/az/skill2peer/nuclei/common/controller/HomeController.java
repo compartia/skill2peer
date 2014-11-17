f3da2afe1eef5539a0741493ede9f403cf7b3ea9
Artem Zaborskii <compartia@gmail.com>
2014-11-17 17:47:33 +0400

    initial

---
+++ src/main/java/org/az/skill2peer/nuclei/common/controller/HomeController.java
@@ -0,0 +1,20 @@
+package org.az.skill2peer.nuclei.common.controller;
+
+import org.az.skill2peer.nuclei.Urls;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showHomePage() {
+        LOGGER.debug("Rendering homepage.");
+        return Urls.VIEW_NAME_HOMEPAGE;
+    }
+}
