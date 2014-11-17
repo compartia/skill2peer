c895471c438c359e0adcfe484c025d620f0b4de8
Artem Zaborskii <compartia@gmail.com>
2014-11-17 21:32:50 +0400

    initial

---
+++ src/integration-test/java/org/az/skill2peer/common/controller/HomeControllerTest.java
@@ -0,0 +1,46 @@
+package org.az.skill2peer.common.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.az.skill2peer.nuclei.UnitTestContext;
+import org.az.skill2peer.nuclei.config.WebAppContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = { UnitTestContext.class, WebAppContext.class })
+@WebAppConfiguration
+@IntegrationTest
+public class HomeControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .build();
+    }
+
+    @Test
+    public void showHomePage_ShouldRenderHomeView() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/index.jsp"));
+    }
+}
