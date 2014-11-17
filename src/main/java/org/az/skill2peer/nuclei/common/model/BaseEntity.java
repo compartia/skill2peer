9c3977e8b3de041b6433b3058ec53d542c3dd660
Artem Zaborskii <compartia@gmail.com>
2014-11-17 15:01:23 +0400

    initial

---
+++ src/main/java/org/az/skill2peer/nuclei/common/model/BaseEntity.java
@@ -0,0 +1,51 @@
+package org.az.skill2peer.nuclei.common.model;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity<ID> {
+
+    @Column(name = "creation_time", nullable = false)
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime creationTime;
+
+    @Column(name = "modification_time", nullable = false)
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime modificationTime;
+
+    @Version
+    private long version;
+
+    public DateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public abstract ID getId();
+
+    public DateTime getModificationTime() {
+        return modificationTime;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        final DateTime now = DateTime.now();
+        this.creationTime = now;
+        this.modificationTime = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.modificationTime = DateTime.now();
+    }
+}
