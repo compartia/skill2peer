25a03e468fb127a1ee9ec9cfb9653293e09f6fb4
Artem Zaborskii <compartia@gmail.com>
2014-11-17 21:12:15 +0400

    initial

---
+++ src/test/java/net/petrikainulainen/spring/social/signinmvc/TestUtil.java
@@ -0,0 +1,16 @@
+package net.petrikainulainen.spring.social.signinmvc;
+
+public class TestUtil {
+
+    private static final String CHARACTER = "a";
+
+    public static String makeStringOfLen(final int length) {
+        final StringBuilder builder = new StringBuilder();
+
+        for (int index = 0; index < length; index++) {
+            builder.append(CHARACTER);
+        }
+
+        return builder.toString();
+    }
+}
