cb2e0f158a93a7bbad9fec9bbd00c71544daea4e
Artem Zaborskii <compartia@gmail.com>
2014-11-18 15:33:52 +0400

    save method

--- src/main/java/org/az/skill2peer/nuclei/common/controller/rest/CourseRestController.java
+++ src/main/java/org/az/skill2peer/nuclei/common/controller/rest/CourseRestController.java
@@ -9,16 +9,13 @@ import org.az.skill2peer.nuclei.common.controller.rest.dto.CourseEditDto;
 import org.az.skill2peer.nuclei.common.controller.rest.dto.CourseMetaDataDto;
 import org.az.skill2peer.nuclei.common.model.Course;
 import org.az.skill2peer.nuclei.common.model.CourseFavorite;
-import org.az.skill2peer.nuclei.common.model.CourseStatus;
-import org.az.skill2peer.nuclei.common.model.HasOwner;
 import org.az.skill2peer.nuclei.security.util.SecurityUtil;
-import org.az.skill2peer.nuclei.user.model.User;
+import org.az.skill2peer.nuclei.services.CourseService;
 import org.dozer.Mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,13 +29,14 @@ public class CourseRestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CourseRestController.class);
 
-    protected static final String VIEW_NAME_HOMEPAGE = "index";
-
     @PersistenceContext
     EntityManager em;
 
     @Autowired
     Mapper mapper;
+
+    @Autowired
+    CourseService courseService;
 
     @Transactional(readOnly = false)
     @RequestMapping(value = "/{id}/favorite", method = RequestMethod.POST)
@@ -59,38 +57,9 @@ public class CourseRestController {
 
     }
 
-    @Transactional(readOnly = false)
-    @RequestMapping(value = "", method = RequestMethod.POST)
-    @ResponseBody
-    public CourseEditDto save(@RequestBody final CourseEditDto courseDto) {
-        Course course;
-        LOGGER.debug("updating course " + courseDto.getId());
-        if (courseDto.getId() != null) {
-            //update existing
-            course = em.find(Course.class, courseDto.getId());
-            assertCurrentUserHasPermission(course);
-            Assert.state(course.getStatus() != CourseStatus.PUBLISHED, "Can not modify published course");
-
-            mapper.map(courseDto, course);
-            course.setStatus(CourseStatus.DRAFT);
-            em.merge(course);
-        } else {
-            course = new Course();
-            mapper.map(courseDto, course);
-            course.setAuthor(em.find(User.class, SecurityUtil.getCurrentUser().getId()));
-            course.setId(null);
-            course.setStatus(CourseStatus.DRAFT);
-            em.persist(course);
-        }
-        final CourseEditDto ret = new CourseEditDto();
-        mapper.map(course, ret);
-        return ret;
-
-    }
-
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public CourseMetaDataDto showHomePage(@PathVariable("id") final Integer id) {
+    public CourseMetaDataDto getCourseById(@PathVariable("id") final Integer id) {
         final CourseMetaDataDto dto = new CourseMetaDataDto();
         final Course c = em.find(Course.class, id);
         dto.setId(c.getId());
@@ -98,8 +67,21 @@ public class CourseRestController {
         return dto;
     }
 
-    private void assertCurrentUserHasPermission(final HasOwner obj) {
-        //XXX: move to a service
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    @ResponseBody
+    public CourseEditDto save(@RequestBody final CourseEditDto courseDto) {
+        Course course;
+
+        if (courseDto.getId() != null) {
+            //update existing
+            course = courseService.updateCourse(courseDto);
+        } else {
+            course = courseService.createCourse(courseDto);
+        }
+
+        final CourseEditDto ret = new CourseEditDto();
+        mapper.map(course, ret);
+        return ret;
     }
 
     private List<CourseFavorite> getCourseFavorites(final Integer courseId, final Integer userId) {
