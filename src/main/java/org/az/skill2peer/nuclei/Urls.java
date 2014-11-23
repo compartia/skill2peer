70f5274833c717214445840d4f55e6d2fb251a29
Artem Zaborskii <compartia@gmail.com>
2014-11-23 14:25:47 +0400

    initial

---
+++ src/main/java/org/az/skill2peer/nuclei/Urls.java
@@ -0,0 +1,24 @@
+package org.az.skill2peer.nuclei;
+
+public final class Urls {
+    public static final String COURSE_CREATE = "/course/create";
+
+    public static final String COURSE_INFO = "/course/info";
+
+    public static final String VIEW_NAME_HOMEPAGE = "index";
+
+    public static final String FILE_UPLOAD = "/upload";
+
+    public static final String USER_SIGNUP = "/signup";
+
+    public static final String USER_REGISTER = "/user/register";
+
+    public static final String JSP_ROOT = "/WEB-INF/jsp/";
+
+    public static final String USER_REGISTRATION_FORM_JSP = JSP_ROOT + "user/registrationForm.jsp";
+
+    public static final String USER_REGISTRATION_FORM = "user/registrationForm";
+
+    public static final String USER_LOGIN_JSP = JSP_ROOT + "user/login.jsp";
+
+}
